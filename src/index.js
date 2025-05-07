4741e76a2ddbf11b08f632df7eea4f604d10de4a
zaianb <zainnylolade@gmail.com>
2025-05-07 01:31:51 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const menu = document.querySelector('#menu');
 
 burger.addEventListener('click', () => {
 if (menu.classList.contains('hidden')) {
-    menu.classList removeEventListener('hidden')
+    menu.classList.remove ('hidden')
 } else {
     menu.classList.add('hidden');
 }
